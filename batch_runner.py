8cee79ce30250a2b572b83360d8b8b4854d28479
Patrick Wang (Chih-Kai Wang) <kk1fff@patrickz.net>
2014-02-19 23:27:38 +0800

    support output 2 images in a file

--- batch_runner.py
+++ batch_runner.py
@@ -1,22 +1,43 @@
 from PIL import Image
 import polaroid, sys, glob, os
 
+concate = True
+
 s = polaroid.Stylizer()
+s.setOutputDimemsion(3600, 2400)
+s.setBorder(100)
 
 file_list = []
 for i in sys.argv[1:]:
     file_list.extend(glob.glob(i))
 
 file_list.sort()
+
 prev_filename = "" # prevent repeat
+count = 0
+outputImg = None
 for i in file_list:
     if prev_filename == i:
         continue
-    prev_filename = i
     fn = os.path.basename(i)
     fp = os.path.dirname(i)
 
     print("Processing: {0}".format(fn))
 
-    s.draw(Image.open(i)).save("M_" + fn)
+    im = s.draw(Image.open(i))
+    if concate:
+        if count % 2 == 0:
+            outputImg = Image.new("RGB", (im.size[0] * 2, im.size[1]))
+            outputImg.paste(im, (0, 0))
+        else:
+            outputImg.paste(im, (im.size[0], 0))
+            outputImg.save("out-" + str(count/2) + ".jpg")
+            outputImg = None
+    else:
+        im.save("M_" + fn)
+    prev_filename = i
+    count = count + 1
 
+if outputImg != None:
+    outputImg.save("out-" + str((count + 1)/2) + ".jpg")
+    outputImg = None
