89d6927b37f4171c3a4e09d299ebed7f295a0e51
Patrick Wang (Chih-Kai Wang) <kk1fff@patrickz.net>
2014-02-20 22:25:39 +0800

    refactor polaroid.py with oop style

--- polaroid.py
+++ polaroid.py
@@ -4,97 +4,119 @@
 from PIL import Image, ImageDraw, ImageColor, ImageFont
 import sys, random, math
 
-CIRCLEBACKCOLOR = ["#ffa0a0", "#ffff99", "#a0a0ff",
-                   "#a0ffa0", "#ffffa0", "#33ccff"]
+class BackgroundBuilder:
+    """
+    Give a input image as a base image to create background.
+    Return a PIL image.
+    """
+    def drawBackground(self, im):
+        raise Exception("Not implemented")
 
-def DrawCircleBackground(im):
-    dr = ImageDraw.Draw(im)
-    dr.rectangle([(0, 0), im.size], "white")
-    dr = None
+class BorderBuilder:
+    """
+    borderOuterSize is a tuple (width, height) which denotes
+    the outer side dimension.
+    """
+    def drawBorder(self, im, borderOuterSize):
+        raise Exception("Not implemented")
 
-    diagonal = int(math.sqrt(float(im.size[0] * im.size[0] + im.size[1] * im.size[1])))
-    maxr = max(diagonal / 20, 10)
-    minr = max(maxr / 2, 5)
-    border = 5 # max(diagonal / 50, 1)
-    color_index = 0
+class CircleBackgroundBuilder(BackgroundBuilder):
+    CIRCLEBACKCOLOR = ["#ffa0a0", "#ffff99", "#a0a0ff",
+                       "#a0ffa0", "#ffffa0", "#33ccff"]
+    def __init__(self):
+        pass
+    def drawBackground(self, im):
+        dr = ImageDraw.Draw(im)
+        dr.rectangle([(0, 0), im.size], "white")
+        dr = None
 
-    for i in range(40):
-        randx = random.randint(0, im.size[0])
-        randy = random.randint(0, im.size[1])
-        randr = random.randint(minr, maxr)
-        imgSize = (randr * 2, randr * 2)
+        diagonal = int(math.sqrt(float(im.size[0] * im.size[0] + im.size[1] * im.size[1])))
+        maxr = max(diagonal / 20, 10)
+        minr = max(maxr / 2, 5)
+        border = 5 # max(diagonal / 50, 1)
+        color_index = 0
 
-        imDrw = Image.new("RGB", imgSize, CIRCLEBACKCOLOR[color_index])
-        color_index = (color_index + 1) % len(CIRCLEBACKCOLOR)
-        mask = Image.new("L", imgSize, 0)
-        drMask = ImageDraw.Draw(mask)
-        drMask.ellipse([(0, 0), imgSize], 150)
-        drMask.ellipse([(border, border),
-                        (imgSize[0] - border, imgSize[1] - border)], 128)
-        im.paste(imDrw, (randx - randr, randy - randr), mask)
-    return im
+        for i in range(40):
+            randx = random.randint(0, im.size[0])
+            randy = random.randint(0, im.size[1])
+            randr = random.randint(minr, maxr)
+            imgSize = (randr * 2, randr * 2)
 
-# frm, to: (r, g, b)
-def buildGradient(frm, to, step):
-    rstep = float(to[0] - frm[0]) / float(step)
-    gstep = float(to[1] - frm[1]) / float(step)
-    bstep = float(to[2] - frm[2]) / float(step)
-    for i in range(step):
-        yield (int(frm[0] + rstep * i),
-               int(frm[1] + gstep * i),
-               int(frm[2] + bstep * i))
+            imDrw = Image.new("RGB", imgSize, self.CIRCLEBACKCOLOR[color_index])
+            color_index = (color_index + 1) % len(self.CIRCLEBACKCOLOR)
+            mask = Image.new("L", imgSize, 0)
+            drMask = ImageDraw.Draw(mask)
+            drMask.ellipse([(0, 0), imgSize], 150)
+            drMask.ellipse([(border, border),
+                            (imgSize[0] - border, imgSize[1] - border)], 128)
+            im.paste(imDrw, (randx - randr, randy - randr), mask)
+        return im
 
-def DrawBackground(im):
-    # compute gradient
-    dr = ImageDraw.Draw(im)
-    count = 0
-    for c in buildGradient(ImageColor.getrgb("#ffffcc"),
-                           ImageColor.getrgb("white"), 100):
-        dr.rectangle([(count, count), (im.size[0]-count, im.size[1]-count)],
-                      fill = c)
-        count = count + 1
-    return im
+class OldStyleBackgroundBuilder(BackgroundBuilder):
+    @staticmethod
+    def buildGradient(frm, to, step):
+    # frm, to: (r, g, b)
+        rstep = float(to[0] - frm[0]) / float(step)
+        gstep = float(to[1] - frm[1]) / float(step)
+        bstep = float(to[2] - frm[2]) / float(step)
+        for i in range(step):
+            yield (int(frm[0] + rstep * i),
+                   int(frm[1] + gstep * i),
+                   int(frm[2] + bstep * i))
 
-def DrawBorder(im, width, height):
-    bdr = Image.new("RGB", (width, height), "#c0c0c0")
-    bdr_alpha = Image.new("L", (width, height), 48)
-    im.paste(bdr, (0, 0), bdr_alpha)
-    return im
+    def drawBackground(self, im):
+        # compute gradient
+        dr = ImageDraw.Draw(im)
+        count = 0
+        for c in self.buildGradient(
+                ImageColor.getrgb("#ffffcc"),
+                ImageColor.getrgb("white"), 100):
+            dr.rectangle([(count, count),
+                          (im.size[0]-count, im.size[1]-count)],
+                         fill = c)
+            count = count + 1
+        return im
 
-# Size of result image.
-LSIDE = 3200 # px
-SSIDE = 2400 # px
-BORDER = 100 # px
-
-# Border around the photo
-THIN_BORDER = 5 #px
-THIN_BORDER_COLOR = "#303030"
-
-# photo ratio. No unit, just ratio
-PLSIDE = 1
-PSSIDE = 1
-
-# Default delegate
-BgDrawingFunc = DrawCircleBackground
-BgDrawingFuncs = [DrawCircleBackground, DrawBackground]
-BorderDrawingFunc = DrawBorder
-
-# Default font setting
-FONTSIZE = 64
+class BlackBorderBuilder(BorderBuilder):
+    def drawBorder(self, im, dimension):
+        width, height = dimension
+        bdr = Image.new("RGB", (width, height), "#c0c0c0")
+        bdr_alpha = Image.new("L", (width, height), 48)
+        im.paste(bdr, (0, 0), bdr_alpha)
+        return im
 
 class Stylizer:
+    # Size of result image.
+    LSIDE = 3200 # px
+    SSIDE = 2400 # px
+    BORDER = 100 # px
+
+    # Border around the photo
+    THIN_BORDER = 5 # px
+    THIN_BORDER_COLOR = "#303030"
+
+    # photo ratio. No unit, just ratio
+    PLSIDE = 1
+    PSSIDE = 1
+
+    # Default delegate
+    BgBuilderClass = OldStyleBackgroundBuilder
+    BdrBuilderClass = BlackBorderBuilder
+
+    # Default font setting
+    FONTSIZE = 64
+
     def __init__(self):
-        self.long_side = LSIDE
-        self.short_side = SSIDE
-        self.border = BORDER
-        self.p_long_side = PLSIDE
-        self.p_short_side = PSSIDE
-        self.bgDrawer = BgDrawingFunc
-        self.bdrDrawer = BorderDrawingFunc
+        self.long_side = self.LSIDE
+        self.short_side = self.SSIDE
+        self.border = self.BORDER
+        self.p_long_side = self.PLSIDE
+        self.p_short_side = self.PSSIDE
+        self.bgBuilder = None
+        self.bdrBuilder = None
         self.font = ImageFont.load_default()
-        self.thin_border = THIN_BORDER
-        self.thin_border_color = THIN_BORDER_COLOR
-        self.is_background_style_rand = True
+        self.thin_border = self.THIN_BORDER
+        self.thin_border_color = self.THIN_BORDER_COLOR
 
     def setOutputDimemsion(self, longSide, shortSide):
         self.long_side = longSide
@@ -107,15 +129,17 @@ class Stylizer:
         self.p_long_side = longSide
         self.p_short_side = shortSide
 
-    # Pass a function that takes a PIL image as a parameter,
-    # and return a PIL image.
-    def setBgDrawer(self, bgDrawer):
-        self.bgDrawer = bgDrawer
+    def setBgBuilder(self, bgBuilder):
+        if not isinstance(bgBuilder, BackgroundBuilder):
+            raise Exception("only instance of subclass of " +
+                            "BackgroundBuilder can be passed to setBgBuilder")
+        self.bgBuilder = bgBuilder
 
-    # Pass function func(image, width, height)
-    # which returns a PIL image.
-    def setBorderDrawer(self, bdrDrawer):
-        self.bdrDrawer = bdrDrawer
+    def setBorderBuilder(self, bdrBuilder):
+        if not isinstance(bgBuilder, BackgroundBuilder):
+            raise Exception("only instance of subclass of " +
+                            "BorderBuilder can be passed to setBorderBuilder")
+        self.bdrBuilder = bdrBuilder
 
     def setFont(self, fontPath, fontSize):
         self.font = ImageFont.truetype(fontPath, fontSize)
@@ -140,9 +164,12 @@ class Stylizer:
         self.is_background_style_rand = isRandom
 
     def draw(self, im, textOnImage = None):
-        # decide background style
-        if self.is_background_style_rand:
-            self.bgDrawer = BgDrawingFuncs[random.randint(0, len(BgDrawingFuncs) - 1)]
+        # Create builder instances is they are not defined.
+        if self.bgBuilder == None:
+            self.bgBuilder = self.BgBuilderClass()
+
+        if self.bdrBuilder == None:
+            self.bdrBuilder = self.BdrBuilderClass()
 
         # Load image
         photo = im
@@ -177,7 +204,7 @@ class Stylizer:
         im_size = (self.short_side, self.long_side) if orientation == 0 else (self.long_side, self.short_side)
         im = Image.new("RGB", im_size)
 
-        im = self.bgDrawer(im)
+        im = self.bgBuilder.drawBackground(im)
 
         # compute target size
         target_width = 0
@@ -198,7 +225,7 @@ class Stylizer:
         else:
             border_box_width = target_width + 2 * self.border
             border_box_heigth = im_size[1]
-        im = self.bdrDrawer(im, border_box_width, border_box_heigth)
+        im = self.bdrBuilder.drawBorder(im, (border_box_width, border_box_heigth))
 
         # draw thin border
         drwThinBorder = ImageDraw.Draw(im)
